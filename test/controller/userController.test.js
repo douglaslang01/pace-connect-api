b20dfea42e8a30ea5d217a33e0a822850a0950ab
douglaslang01 <douglas.lang@outlook.com>
2025-10-31 16:02:44 -0300

    Criando estrutura para implementacao de testes automatizados

---
+++ test/controller/userController.test.js
@@ -0,0 +1,6 @@
+// Bibliotecas
+const request = require('supertest');
+const { expect } = require('chai');
+
+// Aplicação
+const app = require('../../src/app');
---
+++ test/performance/user/login.test.js
@@ -0,0 +1,13 @@
+// Bibliotecas
+import { check, sleep } from 'k6';
+
+//Aplicação
+
+export const options = {
+    iterations: 1
+};
+
+export default function (data) {
+
+    sleep(1);
+}
