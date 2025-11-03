7268cc2c84faa0cf58bfa7e29744f827af184ce8
douglaslang01 <douglas.lang@outlook.com>
2025-11-03 18:33:11 -0300

    adicionando testes de busca de usuarios por id

--- test/controller/users/listUsers.test.js
+++ test/controller/users/listUsers.test.js
@@ -6,37 +6,65 @@ const { expect } = require('chai');
 const app = require('../../../src/app');
 const auth = require('../../Utils/authentication')
 const testCases = require('../../fixtures/listUsers.json');
+const testCasesById = require('../../fixtures/listUsersById.json');
 
-describe(' GET /users - Data Driven', () => {
+
+describe('Busca de Usuários', () => {
     let token;
     before(async () => {
         token = await auth.getToken();
     });
 
-    testCases.forEach(tc => {
-        it(`${tc.id}: ${tc.description}`, async () => {
-            let req = request(app).get('/users');
 
-            if (tc.headers && tc.headers.Authorization) {
-                req = req.set('Authorization', tc.headers.Authorization.replace('<token válido>', token));
-            }
+    describe('Consulta de Usuários - Data Driven', () => {
 
-            if (tc.query) {
-                req = req.query(tc.query);
-            }
-            const res = await req;
+        testCases.forEach(tc => {
+            it(`${tc.id}: ${tc.description}`, async () => {
+                let req = request(app).get('/users');
 
-            expect(res.status).to.equal(tc.expected.status);
-            if (tc.expected.bodyType === 'array') expect(res.body).to.be.an('array');
-            if (tc.expected.bodyType === 'object') expect(res.body).to.be.an('object');
-            if (tc.expected.bodyLength !== undefined) expect(res.body.length).to.equal(tc.expected.bodyLength);
-            if (tc.expected.errorMsg) expect(JSON.stringify(res.body)).to.match(new RegExp(tc.expected.errorMsg, 'i'));
-            if (tc.expected.fields && res.body.length > 0) {
-                tc.expected.fields.forEach(field => {
-                    expect(res.body[0]).to.have.property(field.name).and.to.be.a(field.type);
-                });
-            }
+                if (tc.headers && tc.headers.Authorization) {
+                    req = req.set('Authorization', tc.headers.Authorization.replace('<token válido>', token));
+                }
+
+                const res = await req;
+
+                expect(res.status).to.equal(tc.expected.status);
+                if (tc.expected.bodyType === 'array') expect(res.body).to.be.an('array');
+                if (tc.expected.bodyType === 'object') expect(res.body).to.be.an('object');
+                if (tc.expected.bodyLength !== undefined) expect(res.body.length).to.equal(tc.expected.bodyLength);
+                if (tc.expected.errorMsg) expect(JSON.stringify(res.body)).to.match(new RegExp(tc.expected.errorMsg, 'i'));
+                if (tc.expected.fields && res.body.length > 0) {
+                    tc.expected.fields.forEach(field => {
+                        expect(res.body[0]).to.have.property(field.name).and.to.be.a(field.type);
+                    });
+                }
+            });
         });
+    });
+
+    describe('Consulta de Usuário por ID - Data Driven', () => {
+
+        testCasesById.forEach(tc => {
+            it(`${tc.id}: ${tc.description}`, async () => {
+                let req = request(app).get(`/users/${tc.userId}`);
+
+                if (tc.headers && tc.headers.Authorization) {
+                    req = req.set('Authorization', tc.headers.Authorization.replace('<token válido>', token));
+                }
+
+                const res = await req;
+
+                expect(res.status).to.equal(tc.expected.status);
+                if (tc.expected.bodyType === 'object') expect(res.body).to.be.an('object');
+                if (tc.expected.errorMsg) expect(JSON.stringify(res.body)).to.match(new RegExp(tc.expected.errorMsg, 'i'));
+                if (tc.expected.fields) {
+                    tc.expected.fields.forEach(field => {
+                        expect(res.body).to.have.property(field.name).and.to.be.a(field.type);
+                    });
+                }
+            });
+        });
+
     });
 });
 
